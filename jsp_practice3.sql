0e2061d4f53ea89895c4a7232356404dedfac6e8
Ok Jae Ook <okjaeook98@gmail.com>
2020-07-30 15:22:53 +0900

    Create jsp_practice3.sql

---
+++ jsp_practice3.sql
@@ -0,0 +1,17 @@
+CREATE TABLE board (
+    board_id NUMBER PRIMARY KEY,
+    writer VARCHAR2(45) NOT NULL,
+    title VARCHAR2(100) NOT NULL,
+    content VARCHAR2(300) NULL,
+    create_at DATE DEFAULT SYSDATE
+);
+
+CREATE SEQUENCE bid_seq
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 1000
+    NOCYCLE
+    NOCACHE;
+    
+    drop table board;
+    drop sequence bid_seq;
